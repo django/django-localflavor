6b3b66b8c784c34ad43ec9a4cad41c129fc6a7ef
Jannis Leidel <jannis@leidel.info>
2013-06-03 16:15:57 +0200

    Reverted the changes to AUStateField again.

--- localflavor/au/models.py
+++ localflavor/au/models.py
@@ -2,24 +2,21 @@ from django.db.models.fields import CharField
 from django.utils.translation import ugettext_lazy as _
 
 from . import forms
+from .au_states import STATE_CHOICES
 
 
 class AUStateField(CharField):
     """
-    A model field that is represented by a
-    :class:`~localflavor.au.forms.AUStateSelect`` form field and
+    A model field that is represented with
+    :data:`~localflavor.au.au_states.STATE_CHOICES`` choices and
     stores the three-letter Australian state abbreviation in the database.
     """
     description = _("Australian State")
 
     def __init__(self, *args, **kwargs):
+        kwargs['choices'] = STATE_CHOICES
         kwargs['max_length'] = 3
         super(AUStateField, self).__init__(*args, **kwargs)
-
-    def formfield(self, **kwargs):
-        defaults = {'form_class': forms.AUStateSelect}
-        defaults.update(kwargs)
-        return super(AUStateField, self).formfield(**defaults)
 
 
 class AUPostCodeField(CharField):
