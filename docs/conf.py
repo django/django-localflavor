107f42a1839b74a0a4629f29a6f8974ce88c11d5
Jannis Leidel <jannis@leidel.info>
2013-06-21 19:05:11 +0200

    Fix building the docs.

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 import sys
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'localflavor.tests.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings')
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
