f9574b0fb368bdb2fb84480b600e57d38d07276e
hirokiky <hirokiky@gmail.com>
2013-08-08 20:15:25 +0900

    Changed JP_PREFECTURES to JP_PREFECTURE_CODES as the code of prefecture
    The fixing first elements of JP_PREFECRUES to some another value (in change set `67c9ae7`)
    will break backward compatibility.
    
    So, Added the choices as another value named JP_PREFECTURE_CODES
    and JPPrefecturCodeSelect as it's form.

--- tests/test_jp.py
+++ tests/test_jp.py
@@ -2,12 +2,66 @@ from __future__ import unicode_literals
 
 from django.test import SimpleTestCase
 
-from localflavor.jp.forms import JPPostalCodeField, JPPrefectureSelect
+from localflavor.jp.forms import (JPPostalCodeField, JPPrefectureSelect,
+                                  JPPrefectureCodeSelect)
 
 
 class JPLocalFlavorTests(SimpleTestCase):
     def test_JPPrefectureSelect(self):
         f = JPPrefectureSelect()
+        out = '''<select name="prefecture">
+<option value="hokkaido">Hokkaido</option>
+<option value="aomori">Aomori</option>
+<option value="iwate">Iwate</option>
+<option value="miyagi">Miyagi</option>
+<option value="akita">Akita</option>
+<option value="yamagata">Yamagata</option>
+<option value="fukushima">Fukushima</option>
+<option value="ibaraki">Ibaraki</option>
+<option value="tochigi">Tochigi</option>
+<option value="gunma">Gunma</option>
+<option value="saitama">Saitama</option>
+<option value="chiba">Chiba</option>
+<option value="tokyo">Tokyo</option>
+<option value="kanagawa" selected="selected">Kanagawa</option>
+<option value="niigata">Niigata</option>
+<option value="toyama">Toyama</option>
+<option value="ishikawa">Ishikawa</option>
+<option value="fukui">Fukui</option>
+<option value="yamanashi">Yamanashi</option>
+<option value="nagano">Nagano</option>
+<option value="gifu">Gifu</option>
+<option value="shizuoka">Shizuoka</option>
+<option value="aichi">Aichi</option>
+<option value="mie">Mie</option>
+<option value="shiga">Shiga</option>
+<option value="kyoto">Kyoto</option>
+<option value="osaka">Osaka</option>
+<option value="hyogo">Hyogo</option>
+<option value="nara">Nara</option>
+<option value="wakayama">Wakayama</option>
+<option value="tottori">Tottori</option>
+<option value="shimane">Shimane</option>
+<option value="okayama">Okayama</option>
+<option value="hiroshima">Hiroshima</option>
+<option value="yamaguchi">Yamaguchi</option>
+<option value="tokushima">Tokushima</option>
+<option value="kagawa">Kagawa</option>
+<option value="ehime">Ehime</option>
+<option value="kochi">Kochi</option>
+<option value="fukuoka">Fukuoka</option>
+<option value="saga">Saga</option>
+<option value="nagasaki">Nagasaki</option>
+<option value="kumamoto">Kumamoto</option>
+<option value="oita">Oita</option>
+<option value="miyazaki">Miyazaki</option>
+<option value="kagoshima">Kagoshima</option>
+<option value="okinawa">Okinawa</option>
+</select>'''
+        self.assertHTMLEqual(f.render('prefecture', 'kanagawa'), out)
+
+    def test_JPPrefectureCodeSelect(self):
+        f = JPPrefectureCodeSelect()
         out = '''<select name="prefecture">
 <option value="01">Hokkaido</option>
 <option value="02">Aomori</option>
