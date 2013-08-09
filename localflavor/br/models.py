56672ca03702e688e1b4fb035ae847dcffd07c80
paulobotta <paulonunes@cwi.com.br>
2013-08-09 13:37:25 -0300

    Add ZipCodeField for brazilian models.

--- localflavor/br/models.py
+++ localflavor/br/models.py
@@ -14,3 +14,21 @@ class BRStateField(CharField):
         kwargs['choices'] = STATE_CHOICES
         kwargs['max_length'] = 2
         super(BRStateField, self).__init__(*args, **kwargs)
+
+
+class ZipCodeField(CharField):
+    """
+    A :class:`~django.db.models.CharField` that checks that the value
+    is a valid BR-style Zip Code (in the format ``xxxxx-xxx``).
+    """
+    description = _("Zip Code")
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 9
+        super(ZipCodeField, self).__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs):
+        from localflavor.br.forms import BRZipCodeField
+        defaults = {'form_class': BRZipCodeField}
+        defaults.update(kwargs)
+        return super(ZipCodeField, self).formfield(**defaults)
