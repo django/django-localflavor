a136834d75c5d0f2590484c00b1e25b5ace58b08
Víðir Valberg Guðmundsson <vidir.valberg@orn.li>
2013-09-17 23:47:31 +0200

    Adding DKPhoneNumberField. Totally ripped of from the NOPhoneNumberField one from localflavor/no/.

--- localflavor/dk/forms.py
+++ localflavor/dk/forms.py
@@ -4,7 +4,9 @@ Denmark specific form helpers.
 
 from __future__ import absolute_import, unicode_literals
 
+from django.utils.translation import ugettext_lazy as _
 from django.forms.widgets import Select
+from django.forms.fields import RegexField
 
 from .dk_postalcodes import DK_POSTALCODES
 
@@ -15,3 +17,22 @@ class DKPostalCodeSelect(Select):
     """
     def __init__(self, attrs=None):
         super(DKPostalCodeSelect, self).__init__(attrs, choices=DK_POSTALCODES)
+
+
+class DKPhoneNumberField(RegexField):
+    """
+    Field with phone number validation. Requires a phone number with
+    8 digits and optional country code
+    """
+    default_error_messages = {
+        'invalid': _('A phone number must be 8 digits and may have country code'),
+    }
+
+    def __init__(self, max_length=None, min_length=None, *args, **kwargs):
+        super(DKPhoneNumberField, self).__init__(
+            r'^(?:\+47)? ?(\d{3}\s?\d{2}\s?\d{3}|\d{2}\s?\d{2}\s?\d{2}\s?\d{2})$',
+            max_length,
+            min_length,
+            *args,
+            **kwargs
+        )
