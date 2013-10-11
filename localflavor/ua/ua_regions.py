c98678176169f8a78f1faeaf88867bfe14459513
Slava Bacherikov <slava@bacher09.org>
2013-10-11 12:14:19 +0300

    Add Ukrainian local flavor

---
+++ localflavor/ua/ua_regions.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+from django.utils.translation import ugettext_lazy as _
+
+#: https://en.wikipedia.org/wiki/Oblasts_of_Ukraine#List_of_regions
+UA_REGIONS_CHOICES = (
+    ("01", _("Autonomous Republic of Crimea")),
+    ("02", _("Cherkasy Oblast")),
+    ("03", _("Chernihiv Oblast")),
+    ("04", _("Chernivtsi Oblast")),
+    ("05", _("Dnipropetrovsk Oblast")),
+    ("06", _("Donetsk Oblast")),
+    ("07", _("Ivano-Frankivsk Oblast")),
+    ("08", _("Kharkiv Oblast")),
+    ("09", _("Kherson Oblast")),
+    ("10", _("Khmelnytskyi Oblast")),
+    ("11", _("Kiev Oblast")),
+    ("12", _("Kirovohrad Oblast")),
+    ("13", _("Luhansk Oblast")),
+    ("14", _("Lviv Oblast")),
+    ("15", _("Mykolaiv Oblast")),
+    ("16", _("Odessa Oblast")),
+    ("17", _("Poltava Oblast")),
+    ("18", _("Rivne Oblast")),
+    ("19", _("Sumy Oblast")),
+    ("20", _("Ternopil Oblast")),
+    ("21", _("Vinnytsia Oblast")),
+    ("22", _("Volyn Oblast")),
+    ("23", _("Zakarpattia Oblast")),
+    ("23", _("Zaporizhia Oblast")),
+    ("24", _("Zhytomyr Oblast")),
+    ("25", _("Kyiv City")),
+    ("26", _("Sevastopol City")),
+)
