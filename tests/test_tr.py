89be3b21c514d110f33f32ba0c9cc627a539babb
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2015-02-09 07:24:23 +0100

    Fixing the TR test.

--- tests/test_tr.py
+++ tests/test_tr.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.utils import six
-from django.utils.unittest import TestCase
+from django.test import SimpleTestCase
 
 from localflavor.tr.forms import (TRPostalCodeField, TRPhoneNumberField,
                                   TRIdentificationNumberField)
@@ -15,7 +15,7 @@ def assertRaisesRegex(self, *args, **kwargs):
     return getattr(self, _assertRaisesRegex)(*args, **kwargs)
 
 
-class TRLocalFlavorTests(TestCase):
+class TRLocalFlavorTests(SimpleTestCase):
     def test_TRPostalCodeField(self):
         f = TRPostalCodeField()
         self.assertEqual(f.clean("06531"), "06531")
