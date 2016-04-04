a9d0f5b4a15552df2dea5d487aebfa6dd9092805
Ben Konrath <ben@bagu.org>
2016-04-04 15:44:49 +0200

    Fix deprecation warning in ESIdentityCardNumberField on master.

--- localflavor/es/forms.py
+++ localflavor/es/forms.py
@@ -96,11 +96,12 @@ class ESIdentityCardNumberField(RegexField):
                                 (self.cif_types + self.nie_types,
                                  self.nif_control + self.cif_control),
                                 re.IGNORECASE)
-        error_message = self.default_error_messages['invalid%s' %
-                                                    (self.only_nif and '_only_nif' or '')]
-        super(ESIdentityCardNumberField, self).__init__(
-            id_card_re, max_length, min_length,
-            error_message=error_message, *args, **kwargs)
+
+        error_messages = kwargs.get('error_messages') or {}
+        error_messages['invalid'] = self.default_error_messages['invalid%s' % (self.only_nif and '_only_nif' or '')]
+        kwargs['error_messages'] = error_messages
+
+        super(ESIdentityCardNumberField, self).__init__(id_card_re, max_length, min_length, *args, **kwargs)
 
     def clean(self, value):
         super(ESIdentityCardNumberField, self).clean(value)
