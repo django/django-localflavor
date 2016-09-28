73ad5000339c2ab865c65f3aedb9a19d5458cbcc
Luis Alberto Santana <jackboot7@gmail.com>
2016-09-28 16:07:31 -0400

    Updated test for VERegionSelect

--- tests/test_ve.py
+++ tests/test_ve.py
@@ -13,16 +13,16 @@ class VELocalFlavorTests(SimpleTestCase):
 
         out = '''<select name="regiones">
 <option value=""></option>
-<option value="501">Región Capital</option>
-<option value="502">Región Central</option>
-<option value="503">Región de los Llanos</option>
-<option value="504">Región Centro Occidental</option>
-<option value="505">Región Zuliana</option>
-<option value="506">Región de los Andes</option>
-<option value="507">Región Nor-Oriental</option>
-<option value="508" selected="selected">Región Insular</option>
-<option value="509">Región Guayana</option>
-<option value="510">Región Sur Occidental</option>
+<option value="501">Regi\xf3n Capital</option>
+<option value="502">Regi\xf3n Central</option>
+<option value="503">Regi\xf3n de los Llanos</option>
+<option value="504">Regi\xf3n Centro Occidental</option>
+<option value="505">Regi\xf3n Zuliana</option>
+<option value="506">Regi\xf3n de los Andes</option>
+<option value="507">Regi\xf3n Nor-Oriental</option>
+<option value="508" selected="selected">Regi\xf3n Insular</option>
+<option value="509">Regi\xf3n Guayana</option>
+<option value="510">Regi\xf3n Sur Occidental</option>
 </select>'''
 
         self.assertHTMLEqual(f.render('regiones', '508'), out)
