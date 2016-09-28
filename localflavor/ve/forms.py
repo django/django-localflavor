5a8361b6871f5fe1b9c1b12396957d88e64c895d
Luis Alberto Santana <jackboot7@gmail.com>
2016-09-28 15:22:40 -0400

    Fixed widget name in forms.py

--- localflavor/ve/forms.py
+++ localflavor/ve/forms.py
@@ -8,9 +8,9 @@ from django.forms.fields import Select
 from .ve_region import REGION_CHOICES
 
 
-class VERegionsSelect(Select):
+class VERegionSelect(Select):
     """
     A Select widget that uses a list of Venezuelan regions as its choices.
     """
     def __init__(self, attrs=None):
-        super(VERegionsSelect, self).__init__(attrs, choices=REGION_CHOICES)
+        super(VERegionSelect, self).__init__(attrs, choices=REGION_CHOICES)
