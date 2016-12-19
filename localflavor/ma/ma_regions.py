cfc436e99b5d09f03432863077971259f97bd026
Ben Konrath <ben@bagu.org>
2016-12-19 15:41:29 +0100

    Mark ma region and province choices as translatable.
    Ref: #270

--- localflavor/ma/ma_regions.py
+++ localflavor/ma/ma_regions.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.utils.translation import ugettext_lazy as _
+
 #: An alphabetical list of regions for use as `choices` in a formfield.
 #: http://www.pncl.gov.ma/fr/Pages/decoupage.aspx
 REGION_CHOICES = (
-    ('01', 'Tanger-Tétouan-Al Hoceïma'),
-    ('02', 'L’Oriental'),
-    ('03', 'Fès-Meknès'),
-    ('04', 'Rabat-Salé-Kénitra'),
-    ('05', 'Béni Mellal-Khénifra'),
-    ('06', 'Casablanca-Settat'),
-    ('07', 'Marrakech-Safi'),
-    ('08', 'Drâa-Tafilalet'),
-    ('09', 'Souss-Massa'),
-    ('10', 'Guelmim-Oued Noun'),
-    ('11', 'Laâyoune-Sakia El Hamra'),
-    ('12', 'Dakhla-Oued Ed Dahab')
+    ('01', _('Tanger-Tétouan-Al Hoceïma')),
+    ('02', _('L’Oriental')),
+    ('03', _('Fès-Meknès')),
+    ('04', _('Rabat-Salé-Kénitra')),
+    ('05', _('Béni Mellal-Khénifra')),
+    ('06', _('Casablanca-Settat')),
+    ('07', _('Marrakech-Safi')),
+    ('08', _('Drâa-Tafilalet')),
+    ('09', _('Souss-Massa')),
+    ('10', _('Guelmim-Oued Noun')),
+    ('11', _('Laâyoune-Sakia El Hamra')),
+    ('12', _('Dakhla-Oued Ed Dahab')),
 )
