3238773d80f74c046efdc6736ce9d4ba4814d02f
Serafeim Papastefanos <spapas@gmail.com>
2018-07-21 11:32:38 +0300

    Add field for greek Social Security Number (AMKA) (#337)

--- localflavor/gr/forms.py
+++ localflavor/gr/forms.py
@@ -1,10 +1,13 @@
 """Greek-specific forms helpers."""
+import datetime
 import re
 
 from django.core.validators import EMPTY_VALUES
 from django.forms import Field, RegexField, ValidationError
 from django.utils.encoding import force_text
 from django.utils.translation import ugettext_lazy as _
+
+from localflavor.generic.checksums import luhn
 
 
 class GRPostalCodeField(RegexField):
@@ -59,4 +62,45 @@ class GRTaxNumberCodeField(Field):
             mod = 0
         if mod != check:
             raise ValidationError(self.error_messages['invalid'])
+        return val
+
+
+class GRSocialSecurityNumberCodeField(RegexField):
+    """
+    Greek social security number (AMKA) field.
+
+    The allow_test_value option can be used to enable the usage of the
+    non valid 00000000000 (11 zeros) value for testing and development
+    """
+
+    default_error_messages = {
+        'invalid': _('Enter a valid greek social security number (AMKA - 11 digits).'),
+    }
+
+    def __init__(self, allow_test_value=False, *args, **kwargs):
+        self.allow_test_value = allow_test_value
+        super(GRSocialSecurityNumberCodeField, self).__init__(r'^[0-9\s\-]+$', *args, **kwargs)
+
+    def check_date(self, val):
+        try:
+            datetime.datetime.strptime(val[:6], '%d%m%y')
+        except:
+            raise ValidationError(self.error_messages['invalid'])
+
+    def clean(self, value):
+        super(GRSocialSecurityNumberCodeField, self).clean(value)
+        if value in self.empty_values:
+            return self.empty_value
+        val = re.sub('[\-\s]', '', force_text(value))
+        if not val or len(val) < 11:
+            raise ValidationError(self.error_messages['invalid'])
+        if self.allow_test_value and val == '00000000000':
+            return val
+        if not all(char.isdigit() for char in val):
+            raise ValidationError(self.error_messages['invalid'])
+
+        self.check_date(val)
+        if not luhn(val):
+            raise ValidationError(self.error_messages['invalid'])
+
         return val
