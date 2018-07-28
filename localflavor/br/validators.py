f029f03387a069fdc050f8051b5e0f4d2971ac48
André Ramos <andrel.ramos97@gmail.com>
2018-07-28 00:25:44 -0300

    Using django translation system

--- localflavor/br/validators.py
+++ localflavor/br/validators.py
@@ -1,3 +1,4 @@
+from django.utils.translation import ugettext_lazy as _
 from django.core.validators import RegexValidator
 
 
@@ -6,8 +7,8 @@ class BRCellPhoneValidator(RegexValidator):
 
     def __init__(self, *args, **kwargs):
         self.regex = '^[(]?[0-9]{2}[)]?[\s]?([0-9]{1})?[0-9]{4}[-]?[0-9]{4}$'
-        self.message = 'Celular apenas com números ou no formato (00) 00000-0000'
-        self.code = 'Celular inválido'
+        self.message = _('Enter a phone number in the format 0000000000 or (00) 00000-0000')
+        self.code = _('Invalid phone')
         super().__init__(*args, **kwargs)
 
 
@@ -16,8 +17,8 @@ class BRTelephoneValidator(RegexValidator):
 
     def __init__(self, *args, **kwargs):
         self.regex = '^[(]?[0-9]{2}[)]?[\s]?[0-9]{4}[-]?[0-9]{4}$'
-        self.message = 'Telefone apenas com números ou no formato (00) 0000-0000'
-        self.code = 'Telefone inválido'
+        self.message = _('Enter a telephone in the format 0000000000 or (00) 0000-0000')
+        self.code = 'Invalid telephone'
         super().__init__(*args, **kwargs)
 
 
@@ -26,22 +27,22 @@ class BRZipCodeValidator(RegexValidator):
 
     def __init__(self, *args, **kwargs):
         self.regex = '^[0-9]{5}[-]?[0-9]{3}$'
-        self.message = 'CEP apenas com números ou no formato 00000-000'
-        self.code = 'CEP inválido'
+        self.message = _('Enter a zip code in the format 00000000 or 00000-000.')
+        self.code = _('Invalid Zip Code')
         super().__init__(*args, **kwargs)
 
 
 class BRCNPJValidator(RegexValidator):
     def __init__(self, *args, **kwargs):
         self.regex = '^[0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2}$'
-        self.message = 'CNPJ apenas com números ou no formato 00.000.000/0000-00'
-        self.code = 'CNPJ inválido'
+        self.message = _('Enter a CNPJ in the format 00000000000000 or 00.000.000/0000-00')
+        self.code = _('Invalid CNPJ')
         super().__init__(*args, **kwargs)
 
 
 class BRCPFValidator(RegexValidator):
     def __init__(self, *args, **kwargs):
         self.regex = '^[0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2}$'
-        self.message = 'CPF apenas com números ou no formato 000.000.000-00'
-        self.code = 'CPF inválido'
+        self.message = _('Enter a CPF in the format 00000000000 or 000.000.000-00')
+        self.code = _('Invalid CPF')
         super().__init__(*args, **kwargs)
