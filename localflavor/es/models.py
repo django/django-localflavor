fb4ff2a07a00a8686e210914b06dc5438aad4359
gil-obradors <gil.obradors@gmail.com>
2019-01-29 13:24:41 +0000

    formated

--- localflavor/es/models.py
+++ localflavor/es/models.py
@@ -6,38 +6,38 @@ from .forms import ESPostalCodeField as ESPostalCodeFormField
 
 
 class ESPostalCodeField(CharField):
-  """
-  A model field that stores the five numbers (XXXXX) of Spain Postal Codes
-  
-  Forms represent it as ``form.ESPostalCodeField``
-  """
-  
-  description = _("Spain postal code ( five numbers)")
-  
-  def __init__(self, *args, **kwargs):
-    kwargs['max_length'] = 5
-    super(ESPostalCodeField, self).__init__(*args, **kwargs)
-    
-  def formfield(self, **kwargs):
-    defaults = {'form_class': ESPostalCodeFormField}
-    defaults.update(kwargs)
-    return super(ESPostalCodeField, self).formfield(**defaults)
+    """
+    A model field that stores the five numbers (XXXXX) of Spain Postal Codes
+        
+    Forms represent it as ``form.ESPostalCodeField``
+    """
+
+    description = _("Spain postal code ( five numbers)")
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 5
+        super(ESPostalCodeField, self).__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {'form_class': ESPostalCodeFormField}
+        defaults.update(kwargs)
+        return super(ESPostalCodeField, self).formfield(**defaults)
 
 
 class ESIdentityCardNumberField(CharField):
-  """A model field that stores Spanish NIF/NIE/CIF in format ``XXXXXXXXX``
-  
-  Forms represent it as ``form.ESIdentityCardNumberField`` field.
-  
-  """
-  
-  description = _("Identification National Document")
-  
-  def __init__(self, *args, **kwargs):
-    kwargs['max_length'] = 10
-    super(ESIdentityCardNumberField, self).__init__(*args, **kwargs)
-    
-  def formfield(self, **kwargs):
-    defaults = {'form_class': ESIdentityCardNumberFormField}
-    defaults.update(kwargs)
-    return super(ESIdentityCardNumberField, self).formfield(**defaults)
+    """A model field that stores Spanish NIF/NIE/CIF in format ``XXXXXXXXX``
+
+    Forms represent it as ``form.ESIdentityCardNumberField`` field.
+
+    """
+
+    description = _("Identification National Document")
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 10
+        super(ESIdentityCardNumberField, self).__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {'form_class': ESIdentityCardNumberFormField}
+        defaults.update(kwargs)
+        return super(ESIdentityCardNumberField, self).formfield(**defaults)
