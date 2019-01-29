e6c5998f4df442e5c7fe14059d9d677f8d15cfa8
Gil Obradors <gil.obradors@gmail.com>
2019-01-29 10:08:47 +0100

    Create models.py
    Added models for ES-ID document and ES-PostalCodes

---
+++ localflavor/es/models.py
@@ -0,0 +1,44 @@
+from django.db.models import CharField
+from django.utils.translation import ugettext_lazy as _
+
+from .forms import ESIdentityCardNumberField as ESIdentityCardNumberFormField
+from .forms import ESPostalCodeField as ESPostalCodeFormField
+
+
+
+class ESPostalCodeField(CharField):
+  """
+  A model field that stores the five numbers of Spain Postal Codes
+  """
+  
+  description = _("Spain postal code ( five numbers)")
+  
+  def __init__(self, *args, **kwargs):
+    kwargs['max_length'] = 5
+    super(ESPostalCodeField, self).__init__(*args, **kwargs)
+    
+  def formfield(self, **kwargs):
+    defaults = {'form_class': ESPostalCodeFormField}
+    defaults.update(kwargs)
+    return super(ESPostalCodeField, self).formfield(**defaults)
+
+
+
+class ESIdentityCardNumberField(CharField):
+  """A model field that stores Spanish NIF/NIE/CIF in format ``XXXXXXXXX``
+  
+  Forms represent it as ``form.ESIdentityCardNumberField`` field.
+  
+  """
+  
+  description = _("Identification National Document")
+  
+  def __init__(self, *args, **kwargs):
+    kwargs['max_length'] = 10
+    super(ESIdentityCardNumberField, self).__init__(*args, **kwargs)
+    
+  def formfield(self, **kwargs):
+    defaults = {'form_class': ESIdentityCardNumberFormField}
+    defaults.update(kwargs)
+    return super(ESIdentityCardNumberField, self).formfield(**defaults)
+  
