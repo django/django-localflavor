0992730247f9f13d0d80fb80de87fad48fd54239
Ben Konrath <ben@bagu.org>
2019-03-23 22:10:32 +0100

    Convert EircodeField to RegexField

--- tests/test_ie.py
+++ tests/test_ie.py
@@ -47,16 +47,27 @@ class IELocalFlavorTests(SimpleTestCase):
             " f28 e50f ": "F28E50F",
         }
         invalid = {
-            "A65F 4E2": error_invalid,
-            "A65  F4E2": error_invalid,
-            "A 65 F4E2": error_invalid,
+            "A65F 4EI": error_invalid,
+            "A65  F4EI": error_invalid,
+            "A 65 F4EI": error_invalid,
             "D4W 1234": error_invalid,
             "Z99ABCF": error_invalid,
             "a65 123b": error_invalid,
             " b0gUS": error_invalid,
         }
         self.assertFieldOutput(EircodeField, valid, invalid)
+
+    def test_EircodeField_error_message_can_be_overridden(self):
         valid = {}
         invalid = {"1NV 4L1D": ["Enter a bloody eircode!"]}
         kwargs = {"error_messages": {"invalid": "Enter a bloody eircode!"}}
         self.assertFieldOutput(EircodeField, valid, invalid, field_kwargs=kwargs)
+
+    def test_EircodeField_formatting(self):
+        field = EircodeField()
+        self.assertEqual(field.prepare_value("A65 F4E2"),  "A65 F4E2")
+        self.assertEqual(field.prepare_value("a65f4e2"), "A65 F4E2",)
+        self.assertEqual(field.prepare_value("D6w123A"), "D6W 123A")
+        self.assertEqual(field.prepare_value(" f28 e50f "), "F28 E50F")
+        self.assertEqual(field.prepare_value(None), '')
+        self.assertEqual(field.prepare_value(''), '')
