0ff23bb4080c8ed30a02e70375005cc7d7e724f1
Ben Konrath <ben@bagu.org>
2019-03-23 22:10:32 +0100

    Convert EircodeField to RegexField

--- localflavor/ie/forms.py
+++ localflavor/ie/forms.py
@@ -2,10 +2,7 @@
 
 from __future__ import unicode_literals
 
-import re
-
-from django.forms import ValidationError
-from django.forms.fields import Select, CharField
+from django.forms.fields import Select, RegexField
 from django.utils.translation import ugettext_lazy as _
 
 from .ie_counties import IE_COUNTY_CHOICES
@@ -18,23 +15,36 @@ class IECountySelect(Select):
         super(IECountySelect, self).__init__(attrs, choices=IE_COUNTY_CHOICES)
 
 
-class EircodeField(CharField):
+class EircodeField(RegexField):
     """
     A form field that validates its input is a valid Eircode (Irish postcode).
 
     The value is uppercased and has the internal space removed, if any.
+
+    See page 12 for the validation syntax:
+    https://www.eircode.ie/docs/default-source/Common/prepareyourbusinessforeircode-edition3published.pdf?sfvrsn=2
+
+    .. versionadded:: 2.2
     """
 
     default_error_messages = {"invalid": _("Enter a valid Eircode.")}
-    eircode_regex = re.compile(
-        r"^(D6W|[AC-FHKNPRTV-Y][0-9]{2}) ?([AC-FHKNPRTV-Y0-9]{4})$"
-    )
 
-    def clean(self, value):
-        value = super(EircodeField, self).clean(value)
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('strip', True)
+        super(EircodeField, self).__init__("^(D6W|[AC-FHKNPRTV-Y][0-9]{2})([AC-FHKNPRTV-Y0-9]{4})$",
+                                           *args, **kwargs)
+
+    def to_python(self, value):
+        # The Eircode should be stored as uppercase without spaces (see page 7):
+        # https://www.eircode.ie/docs/default-source/Common/prepareyourbusinessforeircode-edition3published.pdf?sfvrsn=2
+        value = super(EircodeField, self).to_python(value)
         if value in self.empty_values:
             return self.empty_value
-        eircode = value.upper().strip()
-        if not self.eircode_regex.search(eircode):
-            raise ValidationError(self.error_messages["invalid"])
-        return eircode.replace(" ", "")
+        return value.upper().replace(" ", "")
+
+    def prepare_value(self, value):
+        # Display the Eircode with a space.
+        value = self.to_python(value)
+        if value in self.empty_values:
+            return self.empty_value
+        return '{} {}'.format(value[0:3], value[3:8])
