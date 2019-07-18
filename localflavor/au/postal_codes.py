b3323e07337d8918466f4c5c4302717e03d936eb
Eeshan Jaiswal <esjaiswal1@sheffield.ac.uk>
2019-07-18 18:14:53 +0530

    Postal codes for national capital area added

---
+++ localflavor/au/postal_codes.py
@@ -0,0 +1,38 @@
+
+# source = https://worldpostalcode.com/australia/
+
+POSTAL_CODES_NATIONAL_CAPITAL_AREA = (
+    ('Barton' , '221'),
+    ('Beard' , '2620'),
+    ('Belconnen Dc' , '2617'),
+    ('Bonner', '2914'),
+    ('Canberra Airport', '2609'),
+    ('Canberra Mc', '2610'),
+    ('Capital Hill', '2600'),
+    ('Casey', '2913'),
+    ('Coombs', '2611'),
+    ('Coree', '2611'),
+    ('Crace', '2911'),
+    ('Forde', '2914'),
+    ('Franklin', '2913'),
+    ('Harrison', '2914'),
+    ('Hmas Harman', '2600'),
+    ('Jacka', '2914'),
+    ('Kambah Village' , '2902'),
+    ('Kinlyside', '2913'),
+    ('Kowen', '2620'),
+    ('Majura', '2609'),
+    ('Moncrieff','2914'),
+    ('Oaks Estate' ,'2620'),
+    ('Paddys River' , '2620'),
+    ('Stromlo' , '2611'),
+    ( 'Taylor' , '2913'),
+    ('Uriarra Village' , '2611'),
+    ('Wreck Bay' ,'2540'),
+    ('Wright' ,'2611'),
+    ('Hmas Creswell' ,'2540'),
+    ('Jervis Bay' ,'2540'),
+
+)
+
+
