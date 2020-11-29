b1240506cb8be4efd4b113852426eded16a52657
chmodx <chmod777.ke@gmail.com>
2020-11-29 22:09:42 +0300

    chore: add KE Models

---
+++ localflavor/ke/models.py
@@ -0,0 +1,33 @@
+from typing import Any
+from django.db.models import CharField
+from django.utils.translation import gettext_lazy as _
+
+from .forms import (
+    KECompanyRegNumber as KECompanyRegNumberFormField,
+    KECountySelect,
+    KEIDNumber,
+    KEKraPinNumber,
+    KENHIFNumber,
+    KENSSFNumber,
+    KEPassportNumber,
+    KEPostalCodeField as KEPostalCodeFormField,
+)
+
+
+class KEPostalCodeField(CharField):
+    """
+    A model field that stores the Kenyan Postal Codes
+    """
+    description = _("Kenya Postal Code")
+
+    def __init__(self, *args, **kwargs) -> None:
+        kwargs.update(max_length=8)
+        super().__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs) -> Any:
+        defaults = {"form_class": KEPostalCodeFormField}
+        defaults.update(kwargs)
+        return super().formfield(**defaults)
+
+        
+        
