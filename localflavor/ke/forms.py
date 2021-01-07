8c25e12ed97977df97e0504c42eb646cb2d9db47
chmodx <chmod777.ke@gmail.com>
2021-01-07 09:20:57 +0300

    feat: add regex to validate postal code and ID number

--- localflavor/ke/forms.py
+++ localflavor/ke/forms.py
@@ -1,11 +1,15 @@
 """Kenya-specific Form Helpers"""
 
+import re
+
 from django.forms import ValidationError
 from django.forms.fields import RegexField, Select
 from django.utils.translation import gettext_lazy as _
 
 from .ke_counties import COUNTY_CHOICES
 
+ke_id_re = re.compile(r"^\d{7}(?:\d{1})?$")
+ke_po_box_re = re.compile(r"\A\d{5,5}\Z")
 
 class KEPostalCodeField(RegexField):
     """
@@ -14,21 +18,29 @@ class KEPostalCodeField(RegexField):
     """
 
     default_error_messages = {
-        "invalid": _("Enter a valid Postal code in the format XXXXX")
+        "invalid": _("Enter a valid Kenyan Postal code in the format 12345")
     }
 
-    def __init__(self, **kwargs) -> None:
-        """
-        TODO
-        """
-        super().__init__(r"()", **kwargs)
+    def clean(self, value):
+        value = super().clean(value)
+        if value in self.empty_values:
+            return self.empty_value
+
+        # Strip out spaces and dashes
+        value = value.replace(" ", "").replace("-", "")
+        match = re.match(ke_po_box_re, value)
+        if not match:
+            raise ValidationError(self.error_messages.get("invalid"))
+        return value
+
+
 
 
 class KEKraPinNumber(RegexField):
     """
     TODO
 
-    Kenya Revenue Authority PIN Number
+    A form field that validates input as a Kenya Revenue Authority PIN Number
 
     Validates 2 different formats:
 
@@ -46,9 +58,26 @@ class KEIDNumber(RegexField):
 
     Kenya National ID Number
 
+    8 Digits
+
     """
 
-    ...
+    default_error_messages = {
+        "invalid": _("Enter a valid Kenyan ID Number"),
+    }
+
+    def clean(self, value):
+        value = super().clean(value)
+        if value in self.empty_values:
+            return self.empty_value
+
+        # Strip out spaces and dashes
+        value = value.replace(" ", "").replace("-", "")
+        match = re.match(ke_id_re, value)
+
+        if not match:
+            raise ValidationError(self.error_messages.get("invalid"))
+        return value
 
 
 class KEPassportNumber(RegexField):
@@ -85,13 +114,17 @@ class KECompanyRegNumber(RegexField):
     """
     Kenya Companies Reg. Number
     """
+
     ...
+
 
 class KEPayBillNumber(RegexField):
     """
-    MPESA PayBill 
+    MPESA PayBill
     """
+
     ...
+
 
 class KECountySelect(Select):
     """
