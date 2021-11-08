04f5f851fa36c19a9773baaf4f51807dc2009964
sandeepN06 <sandeepbk01@gmail.com>
2021-11-08 11:56:52 +0530

    add new tests for np models

--- tests/test_np/models.py
+++ tests/test_np/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
-from localflavor.np.models import (NPPostalCodeField , NPDistrictField , NPZoneField , NPProvinceField)
+from localflavor.np.models import (NPDistrictField , NPZoneField , NPProvinceField)
 
 class NepalianPlace(models.Model):
-    postal = NPPostalCodeField(blank = True)
     district = NPDistrictField(blank = True)
     zone = NPZoneField(blank = True)
     province = NPProvinceField(blank = True)
--- tests/test_np/tests.py
+++ tests/test_np/tests.py
@@ -1,13 +1,13 @@
-from django.test import TestCase
+from django.test import TransactionTestCase
 
 from localflavor.np.forms import NPDistrictSelect, NPPostalCodeFormField, NPProvinceSelect, NPZoneSelect
 
 from .selectfields_html import districts_select, provinces_select, zones_select
 
-from .forms import NepaliPlaceForm
+from .models import NepalianPlace
 
 
-class NepalDetails(TestCase):
+class NepalDetails(TransactionTestCase):
     """
         This Test class tests all the selectbox
         fields and Postal Code Fields.
@@ -41,43 +41,26 @@ class NepalDetails(TestCase):
         self.assertFieldOutput(NPPostalCodeFormField, valid, invalid)
 
 
-class NepaliPlaceFormTest(TestCase):
-    """
-        This Test class tests the NepaliPlaceForm
-    """
-    def setUp(self):
-        self.form = NepaliPlaceForm({
-            'postal': '12345',
-            'district': 'achham',
-            'zone': 'mahakali',
-            'province': 'bagmati',
-        })
-
-    def test_get_display_methods(self):
-        """Test that the get_*_display() methods are added to the model instances."""
-        place = self.form.save()
+    def test_NPDistrictField(self):
+        place = NepalianPlace()
+        place.district = 'achham'
+        place.clean_fields()
+        place.save()
         self.assertEqual(place.get_district_display(), 'Achham')
+
+    def test_NPZoneField(self):
+        place = NepalianPlace()
+        place.zone = 'mahakali'
+        place.clean_fields()
+        place.save()
         self.assertEqual(place.get_zone_display(), 'Mahakali')
+
+    def test_NPProvinceField(self):
+        place = NepalianPlace()
+        place.province = 'bagmati'
+        place.clean_fields()
+        place.save()
         self.assertEqual(place.get_province_display(), 'Bagmati')
 
-    def test_errors(self):
-        """Test that required NepaliFormFields throw appropriate errors."""
-        form = NepaliPlaceForm({
-            'postal': 'xxx',
-            'district': 'Invalid district',
-            'zone': 'Invalid zone',
-            'province': 'Invalid province',
 
-        })
-        self.assertFalse(form.is_valid())
-        self.assertEqual(
-            form.errors['district'], ['Select a valid choice. Invalid district is not one of the available choices.']
-        )
-        self.assertEqual(
-            form.errors['zone'], ['Select a valid choice. Invalid zone is not one of the available choices.']
-        )
-        self.assertEqual(
-            form.errors['province'], ['Select a valid choice. Invalid province is not one of the available choices.']
-        )
-        self.assertEqual(form.errors['postal'], ['Enter a postal code in format XXXXX'])
 
