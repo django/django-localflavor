df09fc62e988b064bcf8739229abdce99d5ce67e
dishansa <dishansachin99@gmail.com>
2023-07-24 14:57:07 +0530

    add the LKProvinceSelect form field

--- localflavor/lk/forms.py
+++ localflavor/lk/forms.py
@@ -1,7 +1,9 @@
 """Sri Lanka specific Form helpers."""
 
-from django.forms.fields import RegexField
+from django.forms.fields import RegexField, Select
 from django.utils.translation import gettext_lazy as _
+
+from .lk_provinces import PROVINCES
 
 
 class LKPostalCodeFormField(RegexField):
@@ -18,3 +20,13 @@ class LKPostalCodeFormField(RegexField):
 
     def __init__(self, **kwargs):
         super().__init__(r'^\d{5}$', **kwargs)
+
+
+class LKProvinceSelect(Select):
+    """
+    A Select widget with option to select a provinces from
+    list of all provinces of Sri Lanka.
+    """
+
+    def __init__(self, attrs=None):
+        super().__init__(attrs, choices=PROVINCES)
