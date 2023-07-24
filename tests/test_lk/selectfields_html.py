94ee8396448ae0157a7c58747964d4e95346a3cd
dishansa <dishansachin99@gmail.com>
2023-07-24 18:37:38 +0530

    Add tests

---
+++ tests/test_lk/selectfields_html.py
@@ -0,0 +1,46 @@
+"""Option for all select box fields"""
+
+districts_select = """
+<select name="district">
+  <option value="kandy" selected>Kandy</option>
+  <option value="matale">Matale</option>
+  <option value="nuwara_eliya">Nuwara Eliya</option>
+  <option value="anuradhapura">Anuradhapura</option>
+  <option value="polonnaruwa">Polonnaruwa</option>
+  <option value="jaffna">Jaffna</option>
+  <option value="kilinochchi">Kilinochchi</option>
+  <option value="mannar">Mannar</option>
+  <option value="vavuniya">Vavuniya</option>
+  <option value="mullativu">Mullativu</option>
+  <option value="alambil">Alambil</option>
+  <option value="ampara">Ampara</option>
+  <option value="batticaloa">Batticaloa</option>
+  <option value="trincomalee">Trincomalee</option>
+  <option value="kurunagala">Kurunagala</option>
+  <option value="puttalam">Puttalam</option>
+  <option value="galle">Galle</option>
+  <option value="hambanthota">Hambanthota</option>
+  <option value="mathara">Mathara</option>
+  <option value="badulla">Badulla</option>
+  <option value="monaragala">Monaragala</option>
+  <option value="kegalle">Kegalle</option>
+  <option value="rathnapura">Rathnapura</option>
+  <option value="colombo">Colombo</option>
+  <option value="gampaha">Gampaha</option>
+  <option value="kaluthara">Kaluthara</option>
+</select>
+"""
+
+provinces_select = """
+<select name="province">
+  <option value="central" selected>Central</option>
+  <option value="north_central">North Central</option>
+  <option value="northern">Northern</option>
+  <option value="eastern">Eastern</option>
+  <option value="north_western">North Western</option>
+  <option value="southern">Southern</option>
+  <option value="uva">Uva</option>
+  <option value="sabaragamuwa">Sabaragamuwa</option>
+  <option value="western">Western</option>
+</select>
+"""
