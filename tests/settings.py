91eabec54e1c6f4826f05fdd238be1432671ba4e
Ben Konrath <ben@bagu.org>
2026-06-23 09:54:11 +0200

    Fix test failures on Django 6.1a and main
    I also upgraded gh-action checkout and setup-python to v6.

--- tests/settings.py
+++ tests/settings.py
@@ -31,3 +31,11 @@ INSTALLED_APPS = [
 SECRET_KEY = 'spam-spam-spam-spam'
 
 SILENCED_SYSTEM_CHECKS = ('1_7.W001', 'models.W042')
+
+# Django 6.1 changed the blank choice to "- Select an option -" which breaks our tests.
+#
+# USE_BLANK_CHOICE_DASH only works for Django 6.x. For Django >= 7.0 we'll need to set
+# django.db.models.fields.BLANK_CHOICE_LABEL in a test app's ready() method if a global setting for this isn't added.
+#
+# https://github.com/django/django/commit/63c56cda133a85a158502891c40465bc0331d3d9
+USE_BLANK_CHOICE_DASH = True
